a93166aacdb81ba5c80e42e743d5d91fa9657f8a
Tyler-Culp <tculp@ucsd.edu>
2022-05-01 19:17:59 -0700

    Added multiply method and JUnit

--- Multiply.java
+++ Multiply.java
@@ -2,6 +2,5 @@ public class Multiply{
 
   public static int mult(int num1, int num2){
     return (num1 * num2);
-    
-  }  
+  } 
 }
---
+++ multiplyTester.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import java.util.*;
+
+public class multiplyTester{
+
+    @Test
+    public void multiplyTest1(){
+        int ans = Multiply.mult(2, 3);
+        assertEquals(5, ans);
+    }
+}
